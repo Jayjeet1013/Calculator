e6b5025d285ac50d0e95b6dcae2223f5db298007
jayjeet kumar <jayjeet1013@gmail.com>
2022-07-08 13:28:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let string= '';
+let buttons = document.querySelectorAll('.btn');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener(('click'),(e)=>{
+
+       if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value= string;
+       }
+       else if(e.target.innerHTML == 'C'){
+        string = "";
+        document.querySelector('input').value= string;
+       }
+       
+        else {string = string + e.target.innerHTML;
+       document.querySelector('input').value = string;
+        }
+    })
+})
